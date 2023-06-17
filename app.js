30a0cca4aca7a17c8e8a4568c2bb3bfa1eaba76d
jose22Manu <101562512+jose22Manu@users.noreply.github.com>
2023-06-17 22:58:57 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+
+
+// for(i=0; i <= 20 ;i++){
+
+//     document.body.style.background='red';
+//     alert(i);
+// }
+
+//     if(i == 20){
+//         document.body.style.background='none';
+
+//     }
+
+
+function scrollToForm() {
+    const formulario = document.getElementById("miFormulario");
+    formulario.scrollIntoView({ behavior: "smooth" });
+ }
+
+
+
+ 
